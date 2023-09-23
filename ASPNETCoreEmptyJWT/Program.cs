90afbc5548b3e5d1d3d7d2f043b5468ef473263c
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-22 22:51:50 -0500

    add authenticate to API

--- ASPNETCoreEmptyJWT/Program.cs
+++ ASPNETCoreEmptyJWT/Program.cs
@@ -1,3 +1,5 @@
+using System.Security.Claims;
+
 Dictionary<string, List<string>> gamesMap = new()
 {
     {"player1", new List<string>(){"Street Fighter II", "Minecraft"}},
@@ -5,8 +7,35 @@ Dictionary<string, List<string>> gamesMap = new()
 };
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddAuthentication().AddJwtBearer();
+builder.Services.AddAuthorization();
+
 var app = builder.Build();
 
-app.MapGet("/playergames", () => gamesMap);
+
+app.MapGet("/playergames", () => gamesMap)
+    .RequireAuthorization(policy =>
+    {
+        policy.RequireRole("admin");
+    });
+
+app.MapGet("/mygames", (ClaimsPrincipal user) =>
+{
+    ArgumentNullException.ThrowIfNull(user.Identity?.Name);
+
+    var username = user.Identity.Name;
+
+    if(!gamesMap.ContainsKey(username))
+    {
+        return Results.Empty;
+    }
+
+    return Results.Ok(username); ;
+})
+.RequireAuthorization(policy =>
+{
+    policy.RequireRole("player");
+});
 
 app.Run();
