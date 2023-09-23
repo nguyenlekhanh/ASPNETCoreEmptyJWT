237b09eb542828ce8504d1489a34f8e539782d11
Khanh Nguyen <nguyenlekhanh811@gmail.com>
2023-09-22 22:21:10 -0500

    create endpoint /playergames with sample data gamesMap

--- ASPNETCoreEmptyJWT/Program.cs
+++ ASPNETCoreEmptyJWT/Program.cs
@@ -1,6 +1,12 @@
+Dictionary<string, List<string>> gamesMap = new()
+{
+    {"player1", new List<string>(){"Street Fighter II", "Minecraft"}},
+    {"player2", new List<string>(){"Forza Horizon 5", "Final Fantasy XIV", "FIFA 23"}}
+};
+
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapGet("/playergames", () => gamesMap);
 
 app.Run();
